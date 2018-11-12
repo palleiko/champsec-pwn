8d729727cb46ac0f3d72183a6424215a673de58a
palleiko <ian.palleiko@gmail.com>
2018-11-12 17:46:50 -0500

    First Commit

---
+++ basic_bof/bof.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char** argv[]) {
+	int win = 0;
+	char buf[20];
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s INPUT\n", argv[0]);
+		exit(2);
+	}
+
+	strcpy(buf, argv[1]);
+	
+	printf("You entered '%s'\n", buf);
+	if (win) {
+		printf("You Win!\n");
+	} else {
+		printf("Try harder.\n");
+	}
+
+	return 0;
+}
